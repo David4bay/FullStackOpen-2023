1059188ea57a9a91fd3412d588a8ded38fdd9e0c
David4bay <david4bay@gmail.com>
2023-12-08 18:49:58 +0100

    building unicafe statistics portion

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,41 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+
+  return(
+    <table>
+      <StatisticLine text="good" value ={props.good} />
+      <StatisticLine text="neutral" value ={...} />
+      <StatisticLine text="bad" value ={...} />
+      // ...
+    </table>
+  )
+}
+
+const StatisticLine = (props) => {
+
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
+
 const App = () => {
-  // save clicks of each button to its own state
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      code here
+      <br />
+      <Statistics 
+      good={good} 
+      neutral={neutral}
+      bad={bad}
+      />
     </div>
   )
 }
