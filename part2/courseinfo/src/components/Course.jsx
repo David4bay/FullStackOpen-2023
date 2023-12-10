8976c27395a45976e4b481683134062d2de77a27
David4bay <david4bay@gmail.com>
2023-12-10 20:12:28 +0100

    completed courseinfo extended

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,12 +1,47 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
 
+const Header = () => {
+    return (
+        <header>
+            <h1>Web development curriculum</h1>
+        </header>
+    )
+}
 
-const Course = ({couse}) => {
-    
+const Part = ({name, parts}) => {
+    return (
+        <>
+        <h2>{name}</h2>
+        <div>
+        {parts.map((item) => (
+            <React.Fragment key={item.name}>
+                    <p>{item.name} {item.exercises}</p>
+            </React.Fragment>
+            ))}
+        </div>
+        </>
+    )
+}
+
+const Content = ({courses}) => {
+    return (
+        <>
+        {courses.map(({name, parts}) => (
+            <React.Fragment key={name}>
+            <Part name={name} parts={parts} />
+            <strong>total of {parts.reduce((acc, item) => acc + item.exercises, 0)} exercises </strong>
+            </React.Fragment>
+        ))}
+        </>
+    )
+}
+
+const Course = ({courses}) => {
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total />
+            <Header />
+            <Content courses={courses}  />
         </div>
     )
 }
