4e1ee6e35d6a2ec6c0991603f68f2f8dffeb22a6
David4bay <david4bay@gmail.com>
2023-12-15 20:06:22 +0100

    removed conditional bug

--- part2/countryinfo/src/App.jsx
+++ part2/countryinfo/src/App.jsx
@@ -1,14 +1,50 @@
-import Results from "./components/Results/Results"
+import { useState, useEffect, useLayoutEffect } from "react"
 import SearchCountry from "./components/SearchCountry/SearchCountry"
-import { useState } from "react"
+import Results from "./components/Results/Results"
+import axios from 'axios'
+import './App.css'
 
 const App = () => {
 
   const [searchValue, setSearchValue] = useState('')
 
+  const [countryData, setCountryData] = useState(null)
+
+  const [loading, setLoading] = useState(false)
+  
   const handleSearchText = (e) => {
     setSearchValue(e.target.value)
+    setLoading(true)
   }
+
+  useLayoutEffect(() => {
+    if (searchValue.length < 1) {
+      setCountryData(null)
+      setLoading(false)
+    }
+  }, [countryData, searchValue.length])
+
+  
+  useEffect(() => {
+    if (loading) {
+      axios.get('../db.json').then((response) => {
+        console.log(response.data)
+        response.data.filter(({name}) => {
+          if (name.common.toLowerCase() == searchValue.trim() || name.common.includes(searchValue.trim())) {
+            console.log(name.common)
+            setLoading(false)
+            // setCountryData()
+          } else {
+            console.log("too long")
+            setLoading(false)
+          }
+        })
+      }).catch((error) => {
+          console.log("error", error)
+          setLoading(false)
+      })
+    }
+  }, [searchValue, setLoading])
 
   return (
     <div>
@@ -16,7 +52,10 @@ const App = () => {
       searchValue={searchValue}
       handleSearchText={handleSearchText}
       />
-      <Results />
+      <Results 
+      searchedCountries={countryData}
+      loading={loading}
+      />
     </div>
   )
 }
