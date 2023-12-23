db95ff180f964eed2c189b978ee0f3cd2dd8ad03
David4bay <david4bay@gmail.com>
2023-12-23 21:23:05 +0100

    fixed error state

--- part2/savenotes/src/services/notes.js
+++ part2/savenotes/src/services/notes.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/notes'
-const liveUrl = import.meta.env.VITE_LIVE_URL
 
 const getAll = () => {
-  const request = axios.get(liveUrl)
+  const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
