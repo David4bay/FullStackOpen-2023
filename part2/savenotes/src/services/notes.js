960989d1df280528828860faf2c14f90eea2abf2
David4bay <david4bay@gmail.com>
2023-12-23 19:59:35 +0100

    added live url

--- part2/savenotes/src/services/notes.js
+++ part2/savenotes/src/services/notes.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'http://localhost:3001/api/notes'
+const liveUrl = import.meta.env.VITE_LIVE_URL
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
+  const request = axios.get(liveUrl)
   return request.then(response => response.data)
 }
 
