1dff9994c1fb532da336d86a18f71e6921914e50
David4bay <david4bay@gmail.com>
2024-01-18 16:03:03 +0100

    part4 note tests passed

--- part4/nodetut/utils/config.js
+++ part4/nodetut/utils/config.js
@@ -1,7 +1,13 @@
 require('dotenv').config()
 
+const PASSWORD = encodeURIComponent(process.env.MONGO_PASS)
+
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+
+const MONGODB_URI = process.env.NODE_ENV === 'test' ?
+  `mongodb+srv://${process.env.MONGO_USER}:${PASSWORD}@cluster0.7fmaegp.mongodb.net/testNoteApp?retryWrites=true&w=majority` :
+  `mongodb+srv://${process.env.MONGO_USER}:${PASSWORD}@cluster0.7fmaegp.mongodb.net/noteApp?retryWrites=true&w=majority` ||
+  process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
