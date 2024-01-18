02eb592050b48f610a94b551693d9254c411b663
David4bay <david4bay@gmail.com>
2024-01-18 19:46:18 +0100

    passed bloglist test 4.8

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -2,7 +2,9 @@ const PORT = process.env.PORT || 3003
 
 const PASSWORD = encodeURIComponent(process.env.MONGO_PASS)
 
-const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : `mongodb+srv://${process.env.MONGO_USER}:${PASSWORD}@cluster0.7fmaegp.mongodb.net/bloglist?retryWrites=true&w=majority` || process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test' ?
+  `mongodb+srv://${process.env.MONGO_USER}:${PASSWORD}@cluster0.7fmaegp.mongodb.net/testBlogList?retryWrites=true&w=majority` :
+  `mongodb+srv://${process.env.MONGO_USER}:${PASSWORD}@cluster0.7fmaegp.mongodb.net/bloglist?retryWrites=true&w=majority` || process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
