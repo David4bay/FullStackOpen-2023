866c4abcfcd1fdd3937f1748c77540a2b03f31c5
David4bay <david4bay@gmail.com>
2024-01-18 16:28:02 +0100

    part4 new tests passed

--- part4/nodetut/tests/note_api.test.js
+++ part4/nodetut/tests/note_api.test.js
@@ -25,6 +25,18 @@ beforeEach(async () => {
   await noteObject.save()
 }, 100000)
 
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body).toHaveLength(2)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body[0].content).toBe('HTML is easy')
+})
+
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
@@ -52,6 +64,43 @@ test('a specific note is within the returned notes', async () => {
   expect(contents).toContain(
     'Browser can execute only JavaScript'
   )
+})
+
+test('a valid note can be added', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(r => r.content)
+
+  expect(response.body).toHaveLength(initialNotes.length + 1)
+  expect(contents).toContain(
+    'async/await simplifies making async calls'
+  )
+})
+
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400)
+
+  const response = await api.get('/api/notes')
+
+  expect(response.body).toHaveLength(initialNotes.length)
 })
 
 afterAll(async () => {
