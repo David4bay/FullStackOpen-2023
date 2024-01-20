30ed731e40eec13a6da56678a4e1be81b467cf6d
David4bay <david4bay@gmail.com>
2024-01-20 13:58:22 +0100

    login functionality added

--- part4/nodetut/tests/note_api.test.js
+++ part4/nodetut/tests/note_api.test.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-undef */
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
 const api = supertest(app)
 
+const User = require('../models/user')
 const Note = require('../models/note')
 
 // const initialNotes = [
@@ -20,12 +22,43 @@ const Note = require('../models/note')
 
 beforeEach(async () => {
   await Note.deleteMany({})
+  await User.deleteMany({})
 
   for (let note of helper.initialNotes) {
     let noteObject = new Note(note)
     await noteObject.save()
   }
+
+  const passwordHash = await bcrypt.hash('sekret', 10)
+  const user = new User({ username: 'root', passwordHash })
+
+  await user.save()
 }, 120000)
+
+describe('when there is initially one user in db', () => {
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+})
 
 describe('when there is initially some notes saved', () => {
 
