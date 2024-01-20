77b42064061bd498e250793972e675f3cf2e0d95
David4bay <david4bay@gmail.com>
2024-01-20 13:58:22 +0100

    login functionality added

--- part4/nodetut/controllers/notes.js
+++ part4/nodetut/controllers/notes.js
@@ -1,33 +1,38 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 require('express-async-errors')
 
 notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
+  const notes  = Note.find({}).populate('user', { username: 1, name: 1 })
 
-notesRouter.get('/:id', async (request, response) => {
-
-  const note = await Note.findById(request.params.id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  response.json(notes)
 })
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id
   })
 
+  notesRouter.get('/:id', async (request, response) => {
+
+    const note = await Note.findById(request.params.id)
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote.id)
+  await user.save()
   response.status(201).json(savedNote)
 })
 
